0419492a40676790369486cd19526550d74ce220
Dennis <dennisndegwa001@gmail.com>
2023-09-04 16:49:16 +0300

    Latest Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () =>
+  //     import('./home-page/home-page.module').then(
+  //       (m) => m.HomePageModule
+  //     ),
+  // },
+
   {
-    path: '',
+    path: 'share-trading-bot',
     loadChildren: () =>
       import('./home-page/home-page.module').then(
         (m) => m.HomePageModule
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { TestPageComponent } from './test-page/test-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestPageComponent
   ],
   imports: [
     BrowserModule,
